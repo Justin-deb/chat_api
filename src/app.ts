a5785cdc6ab4d88940ef316cdcacd38c2bbcd45e
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-06-06 20:03:18 -0600

    Restructured project

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,13 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import compression from 'compression';
-import helmet from 'helmet';
-import hpp from 'hpp';
+import express from "express";
+import { createServer } from "node:http";
 
-import { ENV } from '#src/config/env.js';
-import { morganMiddleware } from '#src/middlewares/logger.js';
-import { AppError, errorHandler } from '#src/middlewares/error-handler.js';
-import healthcheckRoutes from '#src/modules/healthcheck/healthcheck.route.js';
-import userRoutes from '#src/modules/user/user.route.js';
+const app = express();
+const server = createServer(app);
 
-export const createApp = (): Express => {
-  const app = express();
+app.use(express.json());
 
-  app.use(helmet());
-  app.use(hpp());
+app.get('/',(req,res) =>{
+  res.send('Hello World');
+});
 
-  app.use(
-    cors({
-      origin: ENV.CORS_ORIGIN,
-      credentials: true,
-    }),
-  );
-
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(compression());
-
-  app.use(morganMiddleware);
-
-  app.use('/api', healthcheckRoutes);
-  app.use('/api', userRoutes);
-
-  app.use((_req, _res, next) => {
-    next(new AppError('Route not found', 404));
-  });
-
-  app.use(errorHandler);
-
-  return app;
-};
+server.listen(3000,()=>console.log('server started'))
