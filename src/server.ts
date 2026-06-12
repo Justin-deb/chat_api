4da8e24ff494d337b24ff5d5bb31450e2071b464
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-06-11 20:43:30 -0600

    <Del> removed old project

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "./app.js";
-import { createServer } from "node:http";
-
-const server = createServer(createApp());
-
-
-server.listen(3000,() =>console.log('Server Created'))
